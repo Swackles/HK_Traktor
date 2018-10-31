caa21bbc4a3accff28a733dbdd6361d29707e675
Swackles <Swackles@users.noreply.github.com>
2018-10-31 19:28:42 +0200

    First

--- InfoReader/app.js
+++ InfoReader/app.js
@@ -7,9 +7,6 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
 var app = express();
 
 // view engine setup
@@ -24,8 +21,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+app.use(express.static('public'));
+
+app.get('/traktor', (req, res, next) => {
+    res.sendFile(__dirname + '\\1. kodutöö\\index.html');
+});
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -35,28 +35,6 @@ app.use(function (req, res, next) {
 });
 
 // error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function (err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function (err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
-});
 
 app.set('port', process.env.PORT || 3000);
 
